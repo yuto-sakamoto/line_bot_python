affcbf23f89c41e54f5aff428dc74174138f2637
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2022-04-02 13:14:18 +0900

    add iam policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,41 @@ terraform {
 
 # 変数定義
 variable "aws_region" {}
+variable "aws_profile" {}
+#default_tags
+locals {
+  tags = {
+    project     = "line_bot_python"
+    environment = terraform.workspace
+    terraform   = true
+  }
+}
 # Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+# IAM
+resource "aws_iam_policy" "dynamodb_put_policy" {
+  name        = "dynamodb_put_policy"
+  path        = "/"
+  description = "DynamoDB put policy"
+  tags        = local.tags
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:DescribeTable",
+          "dynamodb:CreateTable",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
